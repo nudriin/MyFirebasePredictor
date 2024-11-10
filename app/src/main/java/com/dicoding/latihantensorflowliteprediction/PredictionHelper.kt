d44cdce0ced197efcc16695fe298fd8ac944f684
Nurdin <mrsunnysummer@gmail.com>
2024-11-11 00:19:33 +0700

    changes: implement firebase model in PredictionHelper

--- app/src/main/java/com/dicoding/latihantensorflowliteprediction/PredictionHelper.kt
+++ app/src/main/java/com/dicoding/latihantensorflowliteprediction/PredictionHelper.kt
@@ -6,6 +6,10 @@ import android.util.Log
 import com.google.android.gms.tflite.client.TfLiteInitializationOptions
 import com.google.android.gms.tflite.gpu.support.TfLiteGpu
 import com.google.android.gms.tflite.java.TfLite
+import com.google.firebase.ml.modeldownloader.CustomModel
+import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions
+import com.google.firebase.ml.modeldownloader.DownloadType
+import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader
 import org.tensorflow.lite.InterpreterApi
 import org.tensorflow.lite.gpu.GpuDelegateFactory
 import java.io.FileInputStream
@@ -19,6 +23,7 @@ class PredictionHelper(
     val context: Context,
     private val onResult: (String) -> Unit,
     private val onError: (String) -> Unit,
+    private val onDownloadSuccess: () -> Unit,
 ) {
 
     private var interpreter: InterpreterApi? = null
@@ -33,19 +38,31 @@ class PredictionHelper(
             }
             TfLite.initialize(context, optionsBuilder.build())
         }.addOnSuccessListener {
-            loadLocalModel()
+            downloadModel()
         }.addOnFailureListener {
             onError(context.getString(R.string.tflite_is_not_initialized_yet))
         }
     }
 
-    private fun loadLocalModel() {
-        try {
-            val buffer: ByteBuffer = loadModelFile(context.assets, modelName)
-            initializeInterpreter(buffer)
-        } catch (ioException: IOException) {
-            ioException.printStackTrace()
-        }
+    @Synchronized
+    private fun downloadModel() {
+        val conditions = CustomModelDownloadConditions.Builder()
+            .requireWifi()
+            .build()
+
+        FirebaseModelDownloader.getInstance()
+            .getModel("Rice-Stock", DownloadType.LOCAL_MODEL, conditions)
+            .addOnSuccessListener { model: CustomModel ->
+                try {
+                    onDownloadSuccess()
+                    initializeInterpreter(model)
+                } catch (e: IOException) {
+                    onError(e.message.toString())
+                }
+            }
+            .addOnFailureListener { _: Exception? ->
+                onError(context.getString(R.string.firebaseml_model_download_failed))
+            }
     }
 
     private fun initializeInterpreter(model: Any) {
@@ -58,6 +75,10 @@ class PredictionHelper(
             }
             if (model is ByteBuffer) {
                 interpreter = InterpreterApi.create(model, options)
+            } else if (model is CustomModel) {
+                model.file?.let {
+                    interpreter = InterpreterApi.create(it, options)
+                }
             }
         } catch (e: Exception) {
             onError(e.message.toString())
